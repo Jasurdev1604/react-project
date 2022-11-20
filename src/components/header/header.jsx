8d1a75f522fce10866d2fca1c71d712330835578
Jasurbek <jasurdev1604@gmail.com>
2022-11-21 00:34:09 +0500

    jasurbek

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css"
+
+const Header = () =>{
+    return(
+        <header>
+            <div className="container">
+                <div className="header">
+                    <h1>Jasurbek O'tebayev</h1>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header

1a22bd2415361d030d157449075ea7d822a1fbf8
Jasurbek <jasurdev1604@gmail.com>
2022-11-21 17:01:23 +0500

    header section

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import "./header.css"
+import "./header.scss";
+
 
 const Header = () =>{
+    
     return(
         <header>
             <div className="container">
                 <div className="header">
-                    <h1>Jasurbek O'tebayev hgbve</h1>
+                     <div className="header__left">
+                     <div className="header__left__logo"><a href="">Medical Core</a></div>
+                     <input type="checkbox" id="input" />
+                    <ul id="nav" className="header__left__nav">
+                        <li className="header__left__nav__li"><a href="#">Home</a></li>
+                        <li className="header__left__nav__li"><a href="#">Product</a></li>
+                        <li className="header__left__nav__li"><a href="#">Pricing</a></li>
+                        <li className="header__left__nav__li"><a href="#">Contact</a></li>
+                    </ul>
+                     </div>
+                    <div className="header__buttons">
+                        <button className="header__buttons__login">Login</button>
+                        <button className="header__buttons__join">join us<i class="fa-solid fa-right-long"></i></button>
+                    </div>
+                    <label for="input" className="bar" id="bar"><i className="fa-solid fa-bars"></i></label>
                 </div>
             </div>
         </header>
     )
 }
-
 export default Header
+
--- src/components/showcase/showcase.jsx
+++ src/components/showcase/showcase.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./showcase.css";
+import "./showcase.scss";
 
 const Showcase = () => {
     return(
         <div className="container">
         <div className="showcase">
-            <h2>Jasurbek O'tebayev hgbve</h2>
+            
         </div>
     </div>
     )
